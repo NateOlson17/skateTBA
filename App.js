78e14eb3f125d84e9f1ae0b1b2cbd1377e23e68c
Nate Olson <nate.8olson@gmail.com>
2020-12-10 22:52:10 -0600

    remedy undefined coord for marker

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
     this.state = {
       APP_WIDTH: Dimensions.get('window').width,
       APP_HEIGHT: Dimensions.get('window').height,
-      plusIconDimensions: 144, //height/width of the plus icon in pixels
+      plusIconDimensions: 144, //height/width of the plus icon in pixels - initialized later - this is fallback value
       darkModeIconDimensions: 57,
       regionState: null, //carries region lat/lon and corresponding deltas
       didMount: false, //tracks component mount status for processes to eliminate memory leakage
@@ -59,11 +59,11 @@ export default class App extends Component {
     this.state.didMount = false; //update state (checked for in _getLocationAsync)
   }
 
-  initiate_addPOI = () => {
+  initiate_addPOI = () => { //when "add POI" button is pressed, triggers this function
     console.log("adding POI")
   }
 
-  darkModeSwitch = () => {
+  darkModeSwitch = () => { //enable dark mode if disabled, and vice versa, called when mode button presse
     if (this.state.darkModeEnabled) {
       console.log("setting dm to false");
       this.state.darkModeEnabled = false;
@@ -74,127 +74,134 @@ export default class App extends Component {
   }
 
   render() {
-      let markerCond = <Marker //marker condition - checked using ternary expression in render()->return() - displayed if regionState defined
+    this.state.plusIconDimensions = this.state.APP_WIDTH * .3; //calculate icon dimensions based on app dimensions
+    this.state.darkModeIconDimensions = this.state.APP_WIDTH * .15;
+    let markerCond = null;
+    if (this.state.regionState) {
+      markerCond = <Marker //marker condition - checked using ternary expression in render()->return() - displayed if regionState defined
                             coordinate = {this.state.regionState}
                             image = {require('./src/components/board.png')}
                             flat = {true}
                         />
-      let defaultMapStyle = [] //////////////////////////////////CREATING MAP STYLES////////////////////////////////////////
-      let darkMapStyle = [
-        {
-          "elementType": "geometry",
-          "stylers": [{"color": "#242f3e"}]
-        },
-        {
-          "elementType": "labels.text.fill",
-          "stylers": [{"color": "#746855"}]
-        },
-        {
-          "elementType": "labels.text.stroke",
-          "stylers": [{"color": "#242f3e"}]
-        },
-        {
-          "featureType": "administrative.locality",
-          "elementType": "labels.text.fill",
-          "stylers": [{"color": "#d59563"}]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "labels.text.fill",
-          "stylers": [{"color": "#d59563"}]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "geometry",
-          "stylers": [{"color": "#263c3f"}]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "labels.text.fill",
-          "stylers": [{"color": "#6b9a76"}]
-        },
-        {
-          "featureType": "road",
-          "elementType": "geometry",
-          "stylers": [{"color": "#38414e"}]
-        },
-        {
-          "featureType": "road",
-          "elementType": "geometry.stroke",
-          "stylers": [{"color": "#212a37"}]
-        },
-        {
-          "featureType": "road",
-          "elementType": "labels.text.fill",
-          "stylers": [{"color": "#9ca5b3"}]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry",
-          "stylers": [{"color": "#746855"}]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [{"color": "#1f2835"}]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.fill",
-          "stylers": [{"color": "#f3d19c"}]
-        },
-        {
-          "featureType": "transit",
-          "elementType": "geometry",
-          "stylers": [{"color": "#2f3948"}]
-        },
-        {
-          "featureType": "transit.station",
-          "elementType": "labels.text.fill",
-          "stylers": [{"color": "#d59563"}]
-        },
-        {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [{"color": "#17263c"}]
-        },
-        {
-          "featureType": "water",
-          "elementType": "labels.text.fill",
-          "stylers": [{"color": "#515c6d"}]
-        },
-        {
-          "featureType": "water",
-          "elementType": "labels.text.stroke",
-          "stylers": [{"color": "#17263c"}]
-        }
-      ]
+    }
+    let defaultMapStyle = [] //generate map styles (stored locally)
+    let darkMapStyle = [
+      {
+        "elementType": "geometry",
+        "stylers": [{"color": "#242f3e"}]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [{"color": "#746855"}]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [{"color": "#242f3e"}]
+      },
+      {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [{"color": "#d59563"}]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [{"color": "#d59563"}]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [{"color": "#263c3f"}]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [{"color": "#6b9a76"}]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [{"color": "#38414e"}]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [{"color": "#212a37"}]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [{"color": "#9ca5b3"}]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [{"color": "#746855"}]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [{"color": "#1f2835"}]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [{"color": "#f3d19c"}]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [{"color": "#2f3948"}]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "labels.text.fill",
+        "stylers": [{"color": "#d59563"}]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{"color": "#17263c"}]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [{"color": "#515c6d"}]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [{"color": "#17263c"}]
+      }
+    ]
     return (
       <View style = {styles.container}>
 
-        <View style = {this.state.darkModeEnabled ? styles.dmheader : styles.header}>
+        <View style = {this.state.darkModeEnabled ? styles.dmheader : styles.header}> 
         </View>
 
         <MapView
           provider = {MapView.PROVIDER_GOOGLE}
           initialRegion = {this.state.regionState}
           zoomEnabled
-          zoomTapEnabled
+          zoomTapEnabled //double tap to zoom
           showsCompass
-          style = {{flex: 1}}
-          customMapStyle = {this.state.darkModeEnabled ? darkMapStyle : defaultMapStyle} //////////////TERNARY TO DETERMINE MAP STYLE/////////////
+          style = {{flex: 1}} //fill parent
+          customMapStyle = {this.state.darkModeEnabled ? darkMapStyle : defaultMapStyle} //ternary determines map style based on darkModeEnabled state
         >
-          {this.state.regionState ? markerCond : null /*conditionally render the markerCond dependent upon the definition status of regionState*/}
+          {markerCond /*conditionally render the markerCond dependent upon the definition status of regionState*/}
         </MapView>
 
         <TouchableOpacity onPress = {this.initiate_addPOI}>
           <Image  //"add POI" button
             style = {{
               position: 'absolute', //positioned absolutely to play nice with map
-              bottom: .04  * this.state.APP_HEIGHT,
-              left: (this.state.APP_WIDTH - this.state.plusIconDimensions)/2
+              bottom: .04  * this.state.APP_HEIGHT, //4% from bottom of screen
+              left: (this.state.APP_WIDTH - this.state.plusIconDimensions)/2, //centered
+              height: this.state.plusIconDimensions, //set using previously calculated icon dimensions
+              width: this.state.plusIconDimensions
             }}
-            source = {this.state.darkModeEnabled ? require('./src/components/dmplus.png') : require('./src/components/plus.png')}
+            source = {this.state.darkModeEnabled ? require('./src/components/dmplus.png') : require('./src/components/plus.png')} //ternary to determine icon based on dark mode
           />
         </TouchableOpacity>
 
@@ -202,10 +209,12 @@ export default class App extends Component {
           <Image  //"mode" button
             style = {{
               position: 'absolute', //positioned absolutely to play nice with map
-              bottom: .045  * this.state.APP_HEIGHT,
-              left: (this.state.APP_WIDTH - this.state.darkModeIconDimensions) / 2 + .25 * this.state.APP_WIDTH
+              bottom: .04  * this.state.APP_HEIGHT, //4% from bottom of screen
+              left: (this.state.APP_WIDTH - this.state.darkModeIconDimensions) / 2 + .25 * this.state.APP_WIDTH, //centered + 25% of width
+              height: this.state.darkModeIconDimensions, //set using previously calculated icon dimensions
+              width: this.state.darkModeIconDimensions
             }}
-            source = {this.state.darkModeEnabled ? require('./src/components/lm.png') : require('./src/components/dm.png')}
+            source = {this.state.darkModeEnabled ? require('./src/components/lm.png') : require('./src/components/dm.png')} //ternary identifies proper icon based on mode
           />
         </TouchableOpacity>
 
